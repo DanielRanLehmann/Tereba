1d46d16e61055b4eb3e3e23d9e3a5e8cfbbf1484
Daniel Ran Lehmann <danielran11@gmail.com>
2017-02-07 23:09:19 +0100

    Initial Commit

---
+++ Tereba/Tereba.h
@@ -0,0 +1,13 @@
+//
+//  Tereba.h
+//  Tereba
+//
+//  Created by Daniel Ran Lehmann on 2/7/17.
+//  Copyright © 2017 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tereba : NSObject
+
+@end
---
+++ Tereba/Tereba.m
@@ -0,0 +1,13 @@
+//
+//  Tereba.m
+//  Tereba
+//
+//  Created by Daniel Ran Lehmann on 2/7/17.
+//  Copyright © 2017 Daniel Ran Lehmann. All rights reserved.
+//
+
+#import "Tereba.h"
+
+@implementation Tereba
+
+@end
